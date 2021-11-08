80bc63c6e520220a4a960311fb0e784324d06fed
1980787 <kunkim1980787@gmail.com>
2021-11-08 15:59:45 -0500

    Initial commit

---
+++ src/Anagram.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Anagram {
+    // dad, add
+    public static void main(String[] args) {
+        char[] str1 = ("Dad").toLowerCase().toCharArray();
+        char[] str2 = ("Add").toLowerCase().toCharArray();
+
+        if (areAnagram(str1, str2)) {
+            System.out.println(Arrays.toString(str1) + " and " + Arrays.toString(str2) + " are anagram.");
+        } else {
+            System.out.println(Arrays.toString(str1) + " and " + Arrays.toString(str2) + " are not anagram.");
+        }
+    }
+
+    static boolean areAnagram(char[] str1, char[] str2) {
+        // capital letter 26 small letter 26
+        // asc 256
+        int[] arr = new int[26];
+        if (str1.length != str2.length) {
+            return false;
+        }
+        for (int i = 0; i < str2.length; i++) {
+            arr[str1[i] - 'a']++;
+            arr[str2[i] - 'a']--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/PrimeNum.java
@@ -0,0 +1,22 @@
+public class PrimeNum {
+    public static void main(String[] args) {
+        int testNum1 = 11;
+        if (isPrime(testNum1)) {
+            System.out.println(testNum1 + " is a prime number.");
+        } else {
+            System.out.println(testNum1 + " is not a prime number.");
+        }
+    }
+
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
