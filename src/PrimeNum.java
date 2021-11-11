3c658b4232466994d0f9630595965823edb0d8c3
1980787 <kunkim1980787@gmail.com>
2021-11-11 10:33:02 -0500

    Replace isPrime code from java7 to java8+

--- src/PrimeNum.java
+++ src/PrimeNum.java
@@ -1,6 +1,8 @@
+import java.util.stream.IntStream;
+
 public class PrimeNum {
     public static void main(String[] args) {
-        int testNum1 = 11;
+        int testNum1 = 7853;
         if (isPrime(testNum1)) {
             System.out.println(testNum1 + " is a prime number.");
         } else {
@@ -11,15 +13,18 @@ public class PrimeNum {
     // convert java 8+
     // using stream
     static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
-        }
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        // Java 7
+//        if (num <= 1) {
+//            return false;
+//        }
+//        for (int i = 2; i < num; i++) {
+//            if (num % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+        // Java 8
+        return num > 1 && IntStream.range(2, num).noneMatch(i -> num % i == 0);
     }
 
 
