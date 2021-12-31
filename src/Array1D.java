2234cbc10dd4a2f73577602aee04cba425263420
1980787 <kunkim1980787@gmail.com>
2021-12-31 16:59:20 -0500

    1D array changed adding java8

--- src/Array1D.java
+++ src/Array1D.java
@@ -1,17 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array1D {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scan.nextInt();
-        }
+//        int n = scan.nextInt();
+//        int[] a = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            a[i] = scan.nextInt();
+//        }
+        int n = Integer.parseInt(scan.nextLine());
+        //                                                        casting
+        int[] a = Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         scan.close();
 
         // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println(a[i]);
         }
         /*
